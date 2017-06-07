70f7f073847537503ca30cf65fa7c1127666a6a5
horngyih <horngyih@yahoo.com>
2017-06-07 22:50:59 +0800

    Initial commit

---
+++ src/main/java/com/ubicompsystem/common/util/functional/App.java
@@ -0,0 +1,13 @@
+package com.ubicompsystem.common.util.functional;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
